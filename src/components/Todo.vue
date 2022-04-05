65592126aaed4f517c5e49bf167843dc66f4a48d
klakenyuo <gillesakakpo01@gmail.com>
2022-04-05 19:29:09 +0100

    finish-v1

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -10,14 +10,29 @@
       </h1>
       <input type="text" @keyup.enter="addTodo" v-model="newTodo" class="w-full  mt-7 p-2 border-gray-300 border-rounded rounded-[5px]" placeholder="Ajouter une tache">
     </header>
-    <main class="mt-6 ">
+    <main class="mt-2 ">
+      <div class="flex "  v-show="toDolength > 0">
+        <input class="mt-2 mr-2" type="checkbox" name="allDone" v-model="allDone">
+        <label for="allDone">tout selectionner</label>
+      </div>
       <ul>
-        <li v-for="todo in todos" v-bind:key="todo.id" class="bg-white p-2 border border-rounded  rounded-[5px]" :class="todo.completed?'line-through' : '' " >
+        <li v-for="todo in filteredTodos" v-bind:key="todo.id" class="bg-white p-2 border border-rounded  rounded-[5px]" :class="todo.completed?'line-through text-gray-400' : '' " >
           <input  type="checkbox" name="{{todo.name}} " v-model="todo.completed"  class="mr-2">
           <label for="{{todo.name}}">{{todo.name}}</label>
+          <button class="ml-[30%] text-red-500 hover:text-gray-300" @click.prevent="deleteTodo(todo)">supprimer</button>
           </li>
       </ul>
     </main>
+    <footer class="mt-3 flex" v-show="toDolength > 0">
+      <span> <strong>{{notdo}}</strong>  {{notdo>1?'taches' : 'tache' }} à faire</span>
+      <div class="ml-[35px]">
+        <ul class="flex">
+          <li class="mr-4 " :class="filter=='all'?'text-blue-900':''" @click.prevent="filter='all'"><a href="#">Toutes</a></li>
+          <li class="mr-4" :class="filter=='todo'?'text-blue-900':''" @click.prevent="filter='todo'"><a href="#">A faire</a></li>
+          <li class="mr-4" :class="filter=='done'?'text-blue-900':''" @click.prevent="filter='done'"><a href="#">Faites</a></li>
+        </ul>
+      </div>
+    </footer>
   </div>
   <div class="w-1/3"></div>
 </div>
@@ -36,7 +51,9 @@
             completed : true
           }
         ],
-        newTodo : ''
+        newTodo : '',
+        allDone : false,
+        filter : 'all'
       }
     },
     methods : {
@@ -50,16 +67,44 @@
           this.newTodo = ''
         }
       },
+      deleteTodo (thetodo) {
+        this.todos = this.todos.filter(todo => todo != thetodo)
+      }
      
      
 
     },
     computed : {
-       
+      allDone : {
+        get() {
+          return this.notdo == 0
+        },
+        set(value) {
+          this.todos.forEach(todo => {
+            todo.completed = value
+          })
+        }
+      },
+       notdo() {
+        return  this.todos.filter(todo => !todo.completed).length
+       },
+       filteredTodos () {
+         if(this.filter == 'todo') {
+           return this.todos.filter(todo => !todo.completed)
+         }else if(this.filter == 'done'){
+            return this.todos.filter(todo => todo.completed)
+         }else{
+           return this.todos
+         }
+       },
+       toDolength () { 
+         return this.todos.length
+       }
+
     }
   }
 </script>
 
 <style >
- 
+  
 </style>
