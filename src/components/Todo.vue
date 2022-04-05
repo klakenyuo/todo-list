5f6fe2ddc8b09cb4d16206ebbeaedac36b78885b
klakenyuo <gillesakakpo01@gmail.com>
2022-04-05 17:33:48 +0100

    tail

---
+++ src/components/Todo.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+defineProps({
+  msg: String
+})
+
+</script>
+
+<template>
+  <div class="text-3xl font-bold underline">
+    Welcome to my app
+  </div>
+</template>
+
+<style >
+ 
+</style>
