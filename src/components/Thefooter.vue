d125fb31170a4a69e3b7c5a5ec5942508c0bc2ce
klakenyuo <gillesakakpo01@gmail.com>
2022-05-20 01:39:58 +0100

    bet

--- src/components/Thefooter.vue
+++ src/components/Thefooter.vue
@@ -1,8 +1,8 @@
 <template>
-     <footer class="lg:pl-9 mt-3 flex w-full" v-show="toDolength > 0">
+     <footer class="lg:pl-9 mt-3 flex w-full justify-between" v-show="toDolength > 0">
       <span class="lg:w-1/3 phone:w-1/2"> <strong>{{remainingTodosCount}}</strong>  {{remainingTodosCount>1?'tasks' : 'task' }} left to do</span>
       <div class="lg:w-1/3 phone:hidden"></div>
-      <div class="lg:w-1/3 phone:w-1/2  ">
+      <div class="lg:w-1/3 phone:w-1/2 flex justify-center ">
           <button class=" text-red-500 hover:text-gray-300" @click.prevent="deleteAll()">delete</button>
       </div>
     </footer>
